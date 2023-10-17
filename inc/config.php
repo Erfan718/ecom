aafd74b5f9716efb9cd4d892f7d31765ce920f95
erfan <khazenierfan@gmail.com>
2023-10-17 13:38:39 +0330

    added files

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database="ecom";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+//check connection
+// echo 'hello';
+?>
